0f485f32fd8d0b858fc28e2d8c5e910e2c7b38d8
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-03-12 07:26:49 +0200

    added create models

--- db/migrate/20210305163143_set_default_values.rb
+++
@@ -1,6 +0,0 @@
-class SetDefaultValues < ActiveRecord::Migration[6.1]
-  def change
-    change_column_default :answers, :correct, false
-    change_column_default :tests, :level, 0
-  end
-end
--- db/migrate/20210312052010_create_answers.rb
+++ db/migrate/20210312052010_create_answers.rb
@@ -1,8 +1,9 @@
 class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
-      t.string :body
-      t.boolean :correct
+      t.text :body, null: false
+      t.references :question, foreigh_key: true
+      t.boolean :correct, default: false, null: false
 
       t.timestamps
     end
