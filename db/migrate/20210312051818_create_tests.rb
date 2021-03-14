087137f5b5c73804643b3490e79665476dfa390e
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-03-14 16:14:36 +0200

    added null: false

--- db/migrate/20210312051818_create_tests.rb
+++ db/migrate/20210312051818_create_tests.rb
@@ -3,7 +3,8 @@ class CreateTests < ActiveRecord::Migration[6.1]
     create_table :tests do |t|
       t.text :title, null: false
       t.integer :level, default: 0
-      t.references :category, foreign_key: true
+      t.references :category, foreign_key: true, null: false
+      t.references :user, foreign_key: true, null: false
 
       t.timestamps
     end
--- db/migrate/20210312052010_create_answers.rb
+++ db/migrate/20210312052010_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
       t.text :body, null: false
-      t.references :question, foreigh_key: true
+      t.references :question, foreigh_key: true, null: false
       t.boolean :correct, default: false, null: false
 
       t.timestamps
