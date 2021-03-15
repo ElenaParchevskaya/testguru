9cd1d6358e36f262866b97fdd68823b2057be887
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-03-15 10:45:50 +0200

    added records in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create([
+  {name: 'Alena', email: 'parchi@pp.by', password: '314'},
+  {name: 'Kate', email: 'bun@pp.by', password: '159'}
+])
+
+Category.create([
+  {title: 'Математика'},
+  {title: 'Русский'},
+  {title: 'Другое'}
+])
+
+Test.create([
+  {title: 'Деление чисел', level: 1, category_id: 1},
+  {title: 'Литература', level: 1, category_id: 1},
+  {title: 'IQ', level: 0, category_id: 2}
+])
+
+Question.create([
+  {body: 'Число 4822x делится на 3 и не делится на 2. Какая цифра должна стоять вместо x?', test_id: 1},
+  {body: 'Укажи лишнее слово', test_id: 2},
+  {body: 'Что тяжелее 1 килограмм пуха или железа', test_id: 3},
+])
+
+Answer.create([
+  {body: '2', question_id: 1},
+  {body: '3', question_id: 1},
+  {body: '5', question_id: 1},
+  {body: 'стихотворение', question_id: 2},
+  {body: 'пословица', question_id: 2},
+  {body: 'песенка', question_id: 2},
+  {body: 'пуха', question_id: 3},
+  {body: 'железа', question_id: 3},
+  {body: 'равны', question_id: 3}
+])
