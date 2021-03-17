0d18a0efe0e1d8fa1fbbed78e70a5163ee381dfd
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-03-17 11:20:34 +0200

    added association. refacted methods in User, Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,12 @@
 class Test < ApplicationRecord
+  has_many :questions, dependent: :destroy
+  has_many :results
+  has_many :users, through: :results
+  belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+
   def self.all_with_category(category)
-    Test.joins("INNER JOIN categories ON tests.category_id = categories.id")
+    joins(:category)
          .where("categories.title = :name", name: category)
          .order(title: :desc)
          .pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  has_many :author, class_name: 'Test', foreign_key: 'user_id'
+  has_many :results, dependent: :delete_all
+  has_many :tests, through: :results
+
   def current_test_with_level(level)
-    Test.joins("INNER JOIN results ON results.test_id = tests.id")
-        .where("results.user_id = #{self.id} AND tests.level = :level", level: level)
+    tests.where(level: level)
   end
 end
