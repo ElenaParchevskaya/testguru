99e4baa84deb7df99281f7e4db0eb3fe29d7e1f9
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-03-18 08:48:05 +0200

    adedd optional: true in model Test. fix question.rb, test.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,10 @@
 class Test < ApplicationRecord
+  belongs_to :category, optional: true
+  belongs_to :authored_tests, class_name: 'User', foreign_key: 'user_id'
+
   has_many :questions, dependent: :destroy
   has_many :results
   has_many :users, through: :results
-  belongs_to :category
-  belongs_to :authored_tests, class_name: 'User', foreign_key: 'user_id'
 
   def self.all_with_category(category)
     joins(:category)
