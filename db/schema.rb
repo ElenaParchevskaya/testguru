3d0cc02a37ce1a2ec2c40e3ade1578ac64c32338
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-03-19 12:45:22 +0200

    AddIndexToTests

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_085120) do
+ActiveRecord::Schema.define(version: 2021_03_19_104317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "add_index_to_tests", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -52,11 +57,10 @@ ActiveRecord::Schema.define(version: 2021_03_15_085120) do
     t.text "title", null: false
     t.integer "level", default: 0
     t.bigint "category_id"
-    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
-    t.index ["user_id"], name: "index_tests_on_user_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
   create_table "users", force: :cascade do |t|
