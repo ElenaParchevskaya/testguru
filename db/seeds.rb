81122deb97665a7179947a3e3ae6709d0b68529e
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-03-19 12:58:31 +0200

    added questions with answers

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,9 @@ questions = Question.create!([
   {body: 'Число 4822x делится на 3 и не делится на 2. Какая цифра должна стоять вместо x?', test: tests[0]},
   {body: 'Укажи лишнее слово', test: tests[1]},
   {body: 'Что тяжелее 1 килограмм пуха или железа', test: tests[2]},
+  {body: "15 / 3 = 5. Что здесь является 'делителем'?", test: tests[0]},
+  {body: 'Имя богатыря из города Мурома?', test: tests[1]}
+  {body: 'Плод, в котором было спрятано платье Золушки?', test: tests[1]}
 ])
 
 Answer.create!([
@@ -31,7 +34,16 @@ Answer.create!([
   {body: 'песенка', question: questions[1], correct: true},
   {body: 'пуха', question: questions[2]},
   {body: 'железа', question: questions[2]},
-  {body: 'равны', question: questions[2], correct: true}
+  {body: 'равны', question: questions[2], correct: true},
+  {body: '15', question: questions[3]},
+  {body: '3', question: questions[3], correct: true},
+  {body: 'его тут нет', question: questions[3]},
+  {body: 'Илья', question: questions[4], correct: true},
+  {body: 'Иван', question: questions[4]},
+  {body: 'там не было богатырей', question: questions[4]}
+  {body: 'Орех', question: questions[5], correct: true},
+  {body: 'Тыква', question: questions[5]},
+  {body: 'Платье было в шкафу', question: questions[5]}
 ])
 
 Result.create([
