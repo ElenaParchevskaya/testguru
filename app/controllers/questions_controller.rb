4d2b25d61f008639dc71e32349165be7451cc480
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-03-24 11:36:38 +0100

    fix questions_controller.rb

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
-  before_action :get_test, only: [:index, :create]
-  before_action :find_question, only: [:show, :destroy]
+  before_action :get_test, only: %i[index create]
+  before_action :find_question, only: %i[show destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_quest_not_found
 
