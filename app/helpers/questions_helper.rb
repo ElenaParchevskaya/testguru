91b4a7f3a1339d86a01ba4c8573ccea9949ddc15
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-03-25 15:58:39 +0100

    added def question_header

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,8 @@
 module QuestionsHelper
+  HEADER = {'new' => 'Создать вопрос',
+             'edit' => 'Редактировать вопрос'}.freeze
+
+  def question_header(test_name)
+    "#{HEADER[action_name]} #{test_name}"
+  end
 end
