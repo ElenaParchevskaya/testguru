2810626358956c40e2a5d159141e22087d118cc7
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-03-26 10:09:47 +0100

    fix Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category, optional: true
-  belongs_to :authored_tests, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
   has_many :questions, dependent: :destroy
   has_many :results
