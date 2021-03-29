8f2a3aae1515c09965bdc3585fbf92611fe73f40
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-03-29 17:32:01 +0200

    fix test_passages_helper.rb

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,9 +1,9 @@
 module TestPassagesHelper
   def result_test(test_passage)
     if test_passage.success?
-      content_tag(:span, "Тест пройден. Результат #{test_passage.result}% правильных ответов", class: 'success')
+      content_tag(:span, "Ура, тест пройден. У тебя #{test_passage.result}% правильных ответов", class: 'success')
     else
-      content_tag(:span, "Тест не пройден. Результат #{test_passage.result}% правильных ответов.", class: 'fail')
+      content_tag(:span, "К сожалению, тест не пройдет. У тебя #{test_passage.result}% правильных ответов.", class: 'fail')
     end
   end
  end
