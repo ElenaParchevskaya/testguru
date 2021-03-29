0183ef7df3d22326beeb6aef64ce5d4f79fe3e3f
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-03-29 16:28:32 +0200

    added test_passages

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,8 +3,8 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
   has_many :questions, dependent: :destroy
-  has_many :results
-  has_many :users, through: :results
+  has_many :test_passages
+  has_many :users, through: :test_passages
 
   validates :title, presence: true,
                     uniqueness: { scope: :level,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_141423) do
+ActiveRecord::Schema.define(version: 2021_03_29_141800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,18 @@ ActiveRecord::Schema.define(version: 2021_03_29_141423) do
     t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
+  create_table "test_passages", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "test_id", null: false
+    t.bigint "question_id", null: false
+    t.integer "correct_questions", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_test_passages_on_question_id"
+    t.index ["test_id"], name: "index_test_passages_on_test_id"
+    t.index ["user_id"], name: "index_test_passages_on_user_id"
+  end
+
   create_table "tests", force: :cascade do |t|
     t.text "title", null: false
     t.integer "level", default: 0
@@ -62,5 +74,8 @@ ActiveRecord::Schema.define(version: 2021_03_29_141423) do
   end
 
   add_foreign_key "questions", "tests"
+  add_foreign_key "test_passages", "questions"
+  add_foreign_key "test_passages", "tests"
+  add_foreign_key "test_passages", "users"
   add_foreign_key "tests", "categories"
 end
