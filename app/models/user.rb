c7fcf724e15dc117382b83064e21367b1ac0af70
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-04-02 12:02:46 +0200

    added module Auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
 
+  include Auth
+
   has_many :authored_tests, class_name: 'Test'
   has_many :test_passages, dependent: :delete_all
   has_many :tests, through: :test_passages
 
-  validates :name, :email, presence: true
-
-  has_secure_password
+  # has_secure_password
 
   def current_test_with_level(level)
     tests.where(level: level)
@@ -14,15 +14,5 @@ class User < ApplicationRecord
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test: test)
-  end
-
-  def authenticate(password_string)
-    digest(password_string) == password_digest ? self : false
-  end
-
-  private
-
-  def digest(string)
-    Digest::SHA1.hexdigest(string)
   end
 end
