5d225eb377a608c509daedf3b4706f89fee6ef87
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-04-05 12:31:39 +0200

    admin views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -16,7 +16,7 @@ class QuestionsController < ApplicationController
   def create
     @question = @test.questions.new(question_params)
     if @question.save
-      redirect_to test_path(@test)
+      redirect_to admin_test_path(@test)
     else
       render :new
     end
@@ -32,7 +32,7 @@ class QuestionsController < ApplicationController
 
   def destroy
     @question.destroy
-    redirect_to test_path(@question.test)
+    redirect_to admin_test_path(@question.test)
   end
 
   private
