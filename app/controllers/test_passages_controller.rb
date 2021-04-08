c5ef552ebeefbbbe5e53d6bd1add863bf76580bd
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-04-08 17:46:21 +0200

    fix gists_controller and test_passages_controller

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -17,11 +17,15 @@ class TestPassagesController < ApplicationController
   end
 
   def gist
-     result = GistQuestionService.new(@test_passage.current_question).call
-     flash = result ? { notice: t('.success', link_gist: link_gist(result)) } : {alert: t('.failure')}
-
-     redirect_to @test_passage, flash
-   end
+    result = GistQuestionService.new(@test_passage.current_question).call
+    if result.success?
+      Gist.create(url: result.html_url, user: current_user, question: @test_passage.current_question)
+      flash[:notice] = t('.success', link_gist: link_gist(result))
+    else
+      flash[:alert] = t('.failure')
+    end
+    redirect_to @test_passage
+  end
 
   private
 
@@ -29,7 +33,7 @@ class TestPassagesController < ApplicationController
     @test_passage = TestPassage.find(params[:id])
   end
 
-  def link_gist(result)
-    view_context.link_to('Gist', result.html_url)
+  def link_gist(gist)
+    view_context.link_to('Gist', gist.html_url, target: '_blank')
   end
 end
