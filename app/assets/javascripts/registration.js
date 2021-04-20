0c2f57c4bb48eaade918e1813720eeb9f6a56e8c
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-04-20 18:19:18 +0300

    validate befor registr user

---
+++ app/assets/javascripts/registration.js
@@ -0,0 +1,67 @@
+document.addEventListener("turbolinks:load", function() {
+
+  signupForm = document.getElementById('new_user')
+  passwordConfirmation = document.getElementById('user_password_confirmation')
+  password = document.getElementById('user_password')
+  email = document.getElementById('user_email')
+  submit = signupForm.getElementsByClassName('btn')[0]
+
+  checker = {
+    email: false,
+    password: false,
+    passwordConfirm: false
+  }
+
+  passwordValidate = function() {
+    if (password.value.length >= 6) {
+      password.classList.remove('error')
+      checker.password = true
+    } else {
+      checker.password = false
+    }
+    return submitInclude()
+  }
+
+  passwordConfirmValidate = function() {
+    if (password.value.length < 6) {
+      password.classList.add('error')
+      return passwordValidate()
+    }
+    if (passwordConfirmation.value === password.value) {
+      checker.passwordConfirm = true
+      password.classList.add('success')
+      passwordConfirmation.classList.add('success')
+    } else {
+      checker.passwordConfirm = false
+      password.classList.remove('success')
+      passwordConfirmation.classList.remove('success')
+    }
+    return submitInclude()
+  }
+
+  emailValidate = function() {
+    if (email.value.match(/.+@.+\..+/i)) {
+      checker.email = true
+      email.classList.add('success')
+    } else {
+      checker.email = false
+      email.classList.remove('success')
+    }
+    return submitInclude()
+  }
+
+  submitInclude = function() {
+    return submit.disabled = !(checker.email && checker.password && checker.passwordConfirm);
+  }
+  if (signupForm && passwordConfirmation) {
+    passwordConfirmation.onkeyup = function() {
+      return passwordConfirmValidate()
+    }
+    password.onkeyup = function() {
+      return passwordValidate()
+    }
+    return email.onkeyup = function() {
+      return emailValidate()
+    }
+  }
+})
