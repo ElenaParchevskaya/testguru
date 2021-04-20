f72ece89024b5146d164c0b73129f84d701146c3
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-04-20 18:17:02 +0300

    sorted test_table

---
+++ app/assets/javascripts/sorting_table_tests.js
@@ -0,0 +1,39 @@
+document.addEventListener("turbolinks:load", function() {
+
+  testNamesColumn = document.getElementById('nameTest')
+
+  setArrow = function(arrow, direct) {
+    return arrow.className = direct < 0 ? 'fa fa-sort-asc' : 'fa fa-sort-desc'
+  }
+
+  sortTable = function(direction) {
+    tbody = document.getElementsByTagName('tbody')[0]
+    trs = tbody.rows
+    trList = [].slice.call(trs)
+
+    trList.sort(function(a, b) {
+      nameA = a.children[0].innerText
+      nameB = b.children[0].innerText
+      if (nameA < nameB) {
+        return -1 * direction
+      }
+      if (nameA > nameB) {
+        return 1 * direction
+      }
+    })
+
+    return trList.forEach(function(tr) {
+      return tbody.append(tr)
+    })
+  }
+
+  if (testNamesColumn) {
+    arrow = testNamesColumn.children[0]
+    direct = 1
+    return testNamesColumn.addEventListener('click', function() {
+      direct *= -1
+      sortTable(direct)
+      return setArrow(arrow, direct)
+    })
+  }
+})
