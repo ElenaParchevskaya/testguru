8e4c9f6881ab5af16e5997e45b4bb5b021282b47
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-05-04 16:52:08 +0300

    fix migration

--- db/migrate/20210504120618_add_devise_to_users.rb
+++ db/migrate/20210504120618_add_devise_to_users.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class AddDeviseToUsers < ActiveRecord::Migration[6.1]
   def self.up
     change_table :users do |t|
@@ -21,8 +19,8 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
       # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
+      # t.inet     :current_sign_in_ip
+      # t.inet     :last_sign_in_ip
 
       ## Confirmable
       t.string   :confirmation_token
@@ -55,6 +53,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
     # By default, we don't want to make any assumption about how to roll back a migration when your
     # model already existed. Please edit below which fields you would like to remove in this migration.
     # raise ActiveRecord::IrreversibleMigration
+
     remove_column         :users, :encrypted_password, :string
     remove_column         :users, :reset_password_token, :string
     remove_column         :users, :reset_password_sent_at, :datetime
