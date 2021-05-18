e7a3ab2d14bb08a91429aa2c098deedfc6fae4cf
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-05-18 16:44:28 +0200

    fix git_hub_client, gist_question_service

--- app/services/gist_question_service.rb
+++ app/services/gist_question_service.rb
@@ -1,22 +1,62 @@
-class GistQuestionService
-  attr_reader :client, :test, :question, :response
+# class GistQuestionService
+#   attr_reader :client, :test, :question, :response
+#
+#   def initialize(question, client: nil)
+#     @client = client || GitHubClient.new
+#     @question = question
+#     @test = @question.test
+#   end
+#
+#   def call
+#     @response = client.create_gist(gist_params)
+#   end
+#
+#   def success?
+#     client.last_response_success?
+#   end
+#
+#   def url_hash
+#     response[:id]
+#   end
+#
+#   private
+#
+#   def http_client
+#     Octokit::Client.new(access_token: ENV['TOKEN_GITHUB_GIST'])
+#   end
+#
+#   def gist_content
+#     content = [@question.body]
+#     content += @question.answers.pluck(:body)
+#     content.join("\n")
+#   end
+#
+#   def gist_params
+#     {
+#       'description': I18n.t('sevices.gist.description',title: @test.title),
+#       'public': true,
+#       'files': {
+#         'test-guru-question.txt': {
+#           content: gist_content
+#         }
+#       }
+#     }
+#   end
+# end
 
-  def initialize(question, client: nil)
-    @client = client || GitHubClient.new
+class GistQuestionService
+
+  ResultObject = Struct.new(:success?, :html_url)
+
+  def initialize(question, client: http_client)
+    @client = client
     @question = question
     @test = @question.test
   end
 
   def call
-    @response = client.create_gist(gist_params)
-  end
-
-  def success?
-    client.last_response_success?
-  end
-
-  def url_hash
-    response[:id]
+    response = @client.create_gist(gist_params)
+    ResultObject.new(response.html_url.present?, response.html_url)
   end
 
   private
