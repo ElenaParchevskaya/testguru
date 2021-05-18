90dc7cd65acdc2132c7089a5e83d575b9f49a171
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-05-18 16:39:00 +0200

    fix migrate

--- db/migrate/20210518075815_create_tests.rb
+++ db/migrate/20210518075815_create_tests.rb
@@ -3,9 +3,9 @@ class CreateTests < ActiveRecord::Migration[6.1]
     create_table :tests do |t|
       t.text :title, null: false
       t.integer :level, default: 0
-      t.references :category, foreign_key: true, null: false
+      t.references :category, foreign_key: true
       t.references :user, foreign_key: true, null: false
-
+      
       t.timestamps
     end
   end
