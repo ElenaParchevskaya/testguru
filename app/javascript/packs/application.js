17639186b1da7ea9a88408280adee3e5137a7c7a
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-05-20 17:19:17 +0200

    added check password

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@
 // that code so it'll be compiled.
 
 require("utilities/sorting")
+require("utilities/check_password_confirmation")
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
