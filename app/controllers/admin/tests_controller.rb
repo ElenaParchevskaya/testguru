38f00313550bc6cd5ed71f0fd9c7d7308fd4a25e
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-05-20 19:21:02 +0200

    fix admin

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,5 +1,5 @@
 class Admin::TestsController < Admin::BaseController
-  before_action :set_test, only: %i[start show edit update destroy]
+  before_action :set_test, only: %i[show edit update destroy]
 
   def index
     @tests = Test.all
@@ -14,8 +14,8 @@ class Admin::TestsController < Admin::BaseController
   end
 
   def create
-    @test = current_user.authored_tests.new(test_params)
-
+    @test = Test.new(test_params)
+    @test.author = current_user
     if @test.save
       redirect_to admin_tests_path
     else
@@ -27,7 +27,7 @@ class Admin::TestsController < Admin::BaseController
 
   def update
     if @test.update(test_params)
-      redirect_to test_path(@test)
+      redirect_to admin_test_path(@test)
     else
       render :edit
     end
@@ -35,15 +35,16 @@ class Admin::TestsController < Admin::BaseController
 
   def destroy
     @test.destroy
-      redirect_to admin_tests_path
+    redirect_to admin_tests_path
   end
+
 
   private
 
   def test_params
     params.require(:test).permit(:title, :category_id, :level)
   end
-
+  
   def set_test
     @test = Test.find(params[:id])
   end
