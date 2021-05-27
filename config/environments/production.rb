f74df3103b48f8c841854766a144b3829dc9acac
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-05-27 10:40:42 +0200

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,8 +64,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "testguru_production"
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.action_mailer.default_url_options = { host: 'testsgguru.herokuapp.com'}
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'testsgguru.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
 
