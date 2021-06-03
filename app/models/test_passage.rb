a6f7b4441f52ec5f118812e57d37225536e3c6cb
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-06-03 13:32:06 +0200

    fix

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -7,22 +7,18 @@ class TestPassage < ApplicationRecord
 
   before_validation :before_validation_set_first_question, on: :create
 
-  scope :successful, -> { where('score >= ?', SUCCES_PERCENTAGE) }
-
   def accept!(answer_ids)
-    return if times_up?
-
     self.correct_questions += 1 if correct_answers?(answer_ids)
     self.current_question = next_question
     save!
   end
 
   def completed?
-    current_question.nil? || times_up?
+    current_question.nil?
   end
 
   def success?
-    score >= PASSING_SCORE
+    result >= SUCCES_PERCENTAGE
   end
 
   def result
@@ -33,33 +29,10 @@ class TestPassage < ApplicationRecord
     test.questions.order(:id).where('id < ?', current_question.id).size + 1
   end
 
-  def cache_result
-    update!(score: result)
-  end
-
-  def times_up?
-    timer_exists? ? false : times_up <= Time.current
-  end
-
-  def left_time
-    return false if timer_exists?
-
-    time = times_up - Time.current
-    time.positive? ? time : 0
-  end
-
-  def timer_exists?
-    test.timer.nil?
-  end
-
   private
 
-  def times_up
-    created_at + test.timer.minutes
-  end
-
   def correct_answers?(answer_ids)
-    answer_ids ? correct_answers.ids.sort == answer_ids.map(&:to_i).sort : false
+    correct_answers.ids.sort == answer_ids&.map(&:to_i)&.sort
   end
 
   def correct_answers
