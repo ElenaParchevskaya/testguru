3c49b92831871b2422097e01caa778fda96cb838
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-06-03 13:07:06 +0200

    test complited

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -21,6 +21,7 @@ class TestPassagesController < ApplicationController
      @test_passage.accept!(params[:answer_ids])
 
      if @test_passage.completed?
+       TestPassageComplitedMailer.test_complited(@test_passage).deliver_now
        redirect_to result_test_passage_path(@test_passage)
      else
        render :show
@@ -32,7 +33,7 @@ class TestPassagesController < ApplicationController
   def link_gist(gist)
     view_context.link_to('Gist', gist.html_url, target: '_blank')
   end
-  
+
   def set_test_passage
     @test_passage = TestPassage.find(params[:id])
   end
