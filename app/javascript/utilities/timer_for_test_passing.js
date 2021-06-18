b5819c1c604d596b0bab38bafc8bd2a983b2aa85
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-06-18 15:47:23 +0100

    fix views

--- app/javascript/utilities/timer_for_test_passing.js
+++ app/javascript/utilities/timer_for_test_passing.js
@@ -1,35 +1,27 @@
 document.addEventListener("turbolinks:load", function() {
-    var startTimer;
-    startTimer = function() {
-      var array, m, s, time, timer;
-      timer = document.getElementById('timer');
-      if (!timer) {
-        return;
-      }
-      time = timer.innerHTML;
-      array = time.split(':');
-      m = array[0];
-      s = array[1];
-      if (s === '00' && m === '00') {
-        window.location.reload();
-        return;
-      }
-      if (m < 10) {
-        m = "0" + m;
-      }
-      if (s === '00') {
-        s = 59;
-        m--;
-      } else {
-        s--;
-      }
-      if (s < 10) {
-        s = "0" + s;
-      }
-      timer.innerHTML = m + ":" + s;
-      return setTimeout(startTimer, 1000);
-    };
-    if (document.getElementById('timer')) {
-      return startTimer();
+  var startTimer;
+  startTimer = function() {
+    var m, s, time, timer, _ref;
+    timer = document.getElementById('timer');
+    time = timer.innerHTML;
+    _ref = time.split(':'), m = _ref[0], s = _ref[1];
+    if (m <= 0 && s <= 0) {
+      document.getElementById('sendAnswer').click();
+      return;
     }
-  });
+    if (s === '0' || s === '00') {
+      s = 59;
+      m--;
+    } else {
+      s--;
+    }
+    if (s < 10) {
+      s = "0" + s;
+    }
+    timer.innerHTML = m + ":" + s;
+    return setTimeout(startTimer, 1000);
+  };
+  if (document.getElementById('timer')) {
+    return startTimer();
+  }
+});
